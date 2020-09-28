86b6d7a69535e3ffd2fa10b76c0e265913013503
blurpesec <hahn.michael.f@gmail.com>
2020-09-28 14:25:10 -0700

    add golem routes and disabled them apart from dev builds

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -102,6 +102,10 @@ const RepTokenMigration = lazy(() =>
   import(/* webpackChunkName: "TokenMigration" */ '@features/RepTokenMigration')
 );
 
+const GolemTokenMigration = lazy(() =>
+  import(/* webpackChunkName: "TokenMigration" */ '@features/GolemTokenMigration')
+);
+
 export interface IAppRoutes {
   [K: string]: IAppRoute;
 }
@@ -331,6 +335,15 @@ export const getStaticAppRoutes = (featureFlags: IFeatureFlags): IAppRoute[] => 
     requireAccounts: true,
     enabled: isTruthy(featureFlags.REP_TOKEN_MIGRATION),
     component: RepTokenMigration
+  },
+  {
+    name: ROUTE_PATHS.GOLEM_TOKEN_MIGRATION.name,
+    title: ROUTE_PATHS.GOLEM_TOKEN_MIGRATION.title,
+    path: ROUTE_PATHS.GOLEM_TOKEN_MIGRATION.path,
+    exact: true,
+    requireAccounts: true,
+    enabled: isTruthy(featureFlags.GOLEM_TOKEN_MIGRATION),
+    component: GolemTokenMigration
   }
 ];
 
